cdbc64f2eb29a62fd15e2c17fef16c3a677de9e9
HarryCollins <harrycollins86@googlemail.com>
2017-07-09 14:59:02 +0100

    Added seed data for market type and market status

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+market_type_list = [
+  [ "Fixed Price", 1 ],
+  [ "Spread", 2 ]
+]
+
+market_type_list.each do |type, id|
+  MarketType.create( mechanism: type, id: id )
+end
+
+market_status_list = [
+  [ "Under Construction", 1 ],
+  [ "Published", 2 ],
+  [ "Complete", 3 ]
+]
+
+market_type_list.each do |status, id|
+  MarketStatus.create( market_status: status, id: id )
+end
