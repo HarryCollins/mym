9bfd070ddbba83d900dcf74c3354652b89f6931e
HarryCollins <harrycollins86@gmail.com>
2017-07-22 13:28:22 +0100

    amend results migtation - should allow it to work with PG

--- db/migrate/20170501181622_create_results.rb
+++ db/migrate/20170501181622_create_results.rb
@@ -2,11 +2,15 @@ class CreateResults < ActiveRecord::Migration[5.0]
   def change
     create_table :results do |t|
       t.boolean :result
-      t.references :winner, index: true, foreign_key: true
-      t.references :loser, index: true, foreign_key: true
-	  t.references :market_outcome, index: true, foreign_key: true
-	  t.decimal :pnl
+      t.references :winner, index: true, index: true
+      t.references :loser, index: true, index: true
+	    t.references :market_outcome, index: true, foreign_key: true
+	    t.decimal :pnl
       t.timestamps
     end
+
+    add_foreign_key :results, :users, column: :winner_id
+    add_foreign_key :results, :users, column: :loser_id
+
   end
 end
