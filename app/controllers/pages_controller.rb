0a6788a63fe2495b2251411766edcd5edce9a794
HarryCollins <harrycollins86@gmail.com>
2017-10-18 20:49:25 +0100

    homepage design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
 	before_action :require_admin_user, only: [:admin_settings]
+	before_action :require_not_logged_in, only: [:home]
 
 	def home
 		
@@ -18,6 +19,10 @@ class PagesController < ApplicationController
 
 	def require_admin_user
 		redirect_to root_path if !admin_user?
+	end
+
+	def require_not_logged_in
+		redirect_to user_path(current_user) if current_user
 	end
 
 end
